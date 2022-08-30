7f969eec7f55080805d937e672251d02b8e1d1f6
Nicholas Finch <ngf4@nau.edu>
2022-08-30 15:49:10 -0700

    original script won't get moved to a folder

--- Userful Batch and Shell Scripts/organizeFiles.sh
+++ Userful Batch and Shell Scripts/organizeFiles.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cp organizeFiles.sh ../
-cd ~/Downloads
+cd "${0%/*}"
 for filename in *; do
   if [[ -f "$filename" ]]; then
-      base=${filename%.*}
-      ext=${filename#$base.}
+    base=${filename%.*}
+    ext=${filename#$base.}
     mkdir -p "${ext}"
-    mv "$filename" "${ext}"
+    if [ "$filename" != "organizeFiles.sh" ]; then
+      mv "$filename" "${ext}"
+    fi
   fi
 done
